1047f9f29a89ccb3a1ff4f3d60971e86c49a5b83
Mark <alnyan@airmail.cc>
2020-09-22 18:45:11 +0300

    Release build is broken

--- qemu.sh
+++ qemu.sh
@@ -2,31 +2,44 @@
 
 set -e
 
+IMAGE="build/image.fat32"
+CARGO_ARGS=""
+QEMU_ARGS=""
+KERNEL="target/x86_64-unknown-none/debug/osdev-amd64"
+
+if [ "$QEMU_DEBUG" = 1 ]; then
+    QEMU_ARGS="$QEMU_ARGS -S"
+fi
+
+if [ "$RELEASE" = 1 ]; then
+    CARGO_ARGS="$CARGO_ARGS --release"
+    KERNEL="target/x86_64-unknown-none/release/osdev-amd64"
+fi
+
 mkdir -p build
 
 if [ ! -d "build/yboot2" ]; then
     git clone https://git.alnyan.me/yggdrasil/yboot2.git build/yboot2
 fi
 
-cargo build
+cargo build $CARGO_ARGS
 pushd build/yboot2
 cargo build
 popd
-
-IMAGE="build/image.fat32"
 
 dd if=/dev/zero of=${IMAGE} bs=1M count=64
 mkfs.vfat -F32 ${IMAGE}
 mmd -i ${IMAGE} ::EFI
 mmd -i ${IMAGE} ::EFI/Boot
 mcopy -i ${IMAGE} build/yboot2/target/x86_64-unknown-uefi/debug/yboot2.efi ::EFI/Boot/bootx64.efi
-mcopy -i ${IMAGE} target/x86_64-unknown-none/debug/osdev-amd64 ::kernel.elf
+mcopy -i ${IMAGE} ${KERNEL} ::kernel.elf
 
 qemu-system-x86_64 \
     -drive readonly,format=raw,if=pflash,file=/usr/share/edk2-ovmf/OVMF_CODE.fd \
     -drive format=raw,file=${IMAGE} \
     -net none \
+    -nographic \
     -s \
     -serial mon:stdio \
     -enable-kvm \
-    -cpu host
+    -cpu host $QEMU_ARGS
