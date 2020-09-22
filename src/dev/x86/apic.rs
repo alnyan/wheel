4559001ccea96b7b5c50448f77c234b8a962695b
Mark <alnyan@airmail.cc>
2020-09-22 09:14:29 +0300

    Use enum as register name for lapic

--- src/dev/x86/apic.rs
+++ src/dev/x86/apic.rs
@@ -1,46 +1,45 @@
 use core::ptr::{write_volatile, read_volatile, null_mut};
 use spin::Mutex;
 
-global_asm!(include_str!("irq_s.S"));
-
-// Must look like a single pointer to ASM world
-#[repr(C)]
 pub struct LocalApic {
     address: usize
 }
 
-pub const REG_ID: usize =           0x20;
-pub const REG_TPR: usize =          0x80;
-pub const REG_EOI: usize =          0xB0;
-pub const REG_SVR: usize =          0xF0;
+#[repr(usize)]
+pub enum Reg {
+    ID = 0x20,
+    TPR = 0x80,
+    EOI = 0xB0,
+    SVR = 0xF0,
 
-pub const REG_LVTT: usize =         0x320;
+    LVTT = 0x320,
 
-pub const REG_TMRINITCNT: usize =   0x380;
-pub const REG_TMRCURRCNT: usize =   0x390;
-pub const REG_TMRDIV: usize =       0x3E0;
+    TMRINITCNT = 0x380,
+    TMRCURRCNT = 0x390,
+    TMRDIV = 0x3E0
+}
 
 impl LocalApic {
     // TODO: limits?
     #[inline(always)]
-    fn write(&mut self, off: usize, value: u32) {
-        unsafe { write_volatile((self.address + off) as *mut u32, value); }
+    fn write(&mut self, reg: Reg, value: u32) {
+        unsafe { write_volatile((self.address + reg as usize) as *mut u32, value); }
     }
 
     #[inline(always)]
-    fn read(&self, off: usize) -> u32 {
-        unsafe { read_volatile((self.address + off) as *mut u32) }
+    fn read(&self, reg: Reg) -> u32 {
+        unsafe { read_volatile((self.address + reg as usize) as *mut u32) }
     }
 
     fn init(&mut self) {
-        let tmp = self.read(REG_SVR);
-        self.write(REG_SVR, tmp | (1 << 8) | 0xFF);
+        let tmp = self.read(Reg::SVR);
+        self.write(Reg::SVR, tmp | (1 << 8) | 0xFF);
 
         // Setup APIC timer
-        self.write(REG_TMRDIV, 0x3);
-        self.write(REG_LVTT, 32 | (1 << 17));
-        self.write(REG_TMRINITCNT, 150000);
-        self.write(REG_TMRCURRCNT, 0);
+        self.write(Reg::TMRDIV, 0x3);
+        self.write(Reg::LVTT, 32 | (1 << 17));
+        self.write(Reg::TMRINITCNT, 150000);
+        self.write(Reg::TMRCURRCNT, 0);
     }
 }
 
@@ -55,6 +54,6 @@ pub fn init(address: usize) {
     *APIC.lock() = LocalApic {
         address: address
     };
-    unsafe { apic_eoi = (address + REG_EOI) as *mut _; }
+    unsafe { apic_eoi = (address + Reg::EOI as usize) as *mut _; }
     APIC.lock().init();
 }
--- src/dev/x86/mod.rs
+++ src/dev/x86/mod.rs
@@ -3,3 +3,5 @@ pub use serial::{COM1, SerialPort};
 
 pub mod apic;
 pub use apic::LocalApic;
+
+pub mod irq;
